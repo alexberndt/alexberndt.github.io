db254c0079b38be1530adf8e41e6b5641abb715c
Alexander <alberndt@kth.se>
2021-03-08 14:25:59 +0100

    added v-binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,38 +29,38 @@
       title="ISS Tracker",
       link="https://iss-tracker.tk",
       description="Real-time tracking of the International Space Station",
-      imagelink="/assets/img/isstracker_90.jpg"
+      imagelink="img/isstracker.jpg"
     ) 
-    //- Project(
-    //-   title="Inverse Flying Pendulum Model Predictive Control",
-    //-   link="./f/pendulum-quad-mpc.pdf",
-    //-   description="A model predictive control design and stability analysis for an inverted pendulum balanced on a quadrotor",
-    //-   imagelink="./f/img/isstracker_90.jpg"
-    //- ) 
-    //- Project(
-    //-   title="Handwritten Digits Classification",
-    //-   link="./f/nist-classification.pdf",
-    //-   description="Pattern recognition project on the analysis of machine learning algorithms to classify handwritten numbers",
-    //-   imagelink="./f/img/nist-ml.png"
-    //- )
-    //- Project(
-    //-   title="Smartphone Indoor Localization",
-    //-   link="./f/phone-sensing.pdf",
-    //-   description="Smartphone-based localization using bayesian classification and the particle filter utilizing WiFi access point signal strength and IMU measurements",
-    //-   imagelink="./f/img/sps-project.png"
-    //- )
-    //- Project(
-    //-   title="Mechanical Gimbal Design for Action Camera",
-    //-   link="./f/mox-design.pdf",
-    //-   description="Detailed mechanical gimbal design and dampening validation for an action camera",
-    //-   imagelink="./f/img/mox-drawing.png"
-    //- )
-    //- Project(
-    //-   title="HiL Simulation for off-road ABS brakes",
-    //-   link="./f/thesis-BEng.pdf",
-    //-   description="Bachelor thesis on the implementation of a Hardware-in-the-Loop setup for off-road ABS algorithm validation",
-    //-   imagelink="./f/img/hil.png"
-    //- )
+    Project(
+      title="Inverse Flying Pendulum Model Predictive Control",
+      link="./f/pendulum-quad-mpc.pdf",
+      description="A model predictive control design and stability analysis for an inverted pendulum balanced on a quadrotor",
+      imagelink="img/quadrotor.jpg"
+    ) 
+    Project(
+      title="Handwritten Digits Classification",
+      link="./f/nist-classification.pdf",
+      description="Pattern recognition project on the analysis of machine learning algorithms to classify handwritten numbers",
+      imagelink="img/nist-ml.png"
+    )
+    Project(
+      title="Smartphone Indoor Localization",
+      link="./f/phone-sensing.pdf",
+      description="Smartphone-based localization using bayesian classification and the particle filter utilizing WiFi access point signal strength and IMU measurements",
+      imagelink="img/sps-project.png"
+    )
+    Project(
+      title="Mechanical Gimbal Design for Action Camera",
+      link="./f/mox-design.pdf",
+      description="Detailed mechanical gimbal design and dampening validation for an action camera",
+      imagelink="img/mox-drawing.png"
+    )
+    Project(
+      title="HiL Simulation for off-road ABS brakes",
+      link="./f/thesis-BEng.pdf",
+      description="Bachelor thesis on the implementation of a Hardware-in-the-Loop setup for off-road ABS algorithm validation",
+      imagelink="img/hil.png"
+    )
 </template>
 
 <script>
