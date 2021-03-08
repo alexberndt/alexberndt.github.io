dff0307e258114cb7ca413debfb06ca73b11990f
Alexander <alberndt@kth.se>
2021-03-08 12:55:04 +0100

    Added some bio stuff

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,8 +1,20 @@
 <template lang="pug">
 .bio
-  img.img-thumbnail.rounded-circle.border-1( :src="require('@/assets/' + imagelink + '')", alt='logo')
-  p {{ text1 }}
-  p {{ text2 }}
+  .container 
+    .row 
+      .col-md-3
+        img.img-thumbnail.rounded-circle.border-1( :src="require('@/assets/' + imagelink + '')", alt='logo')
+      .col-md-9.biotext
+        p {{ text1 }}
+        p {{ text2 }}
+        .container 
+          .row 
+            a.col-md-3.card-link(href="https://github.com/alexberndt")
+              | GitHub
+            a.col-md-3.card-link(href="https://linkedin.com/in/alexander-berndt/")
+                    | LinkedIn
+            a.col-md-3.card-link(href="mailto:alberndt@kth.se")
+                    | E-mail
 </template>
 
 <script>
@@ -17,5 +29,8 @@ export default {
 .img-thumbnail {
   max-height: 150px;
   margin: 0px;
+}
+.biotext {
+  text-align: left;
 }
 </style>
